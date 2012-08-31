6aa540bb59269aab74011198f9f8c1edc0a0050a
Martin Higham <martin@ocastalabs.com>
2012-08-31 16:27:23 +0100

    param change

--- recipes/plugin_suggest.rb
+++ recipes/plugin_suggest.rb
@@ -1,1 +1,1 @@
-install_plugin https://github.com/downloads/spinscale/elasticsearch-suggest-plugin/elasticsearch-suggest-#{node.elasticsearch[:plugin][:aws][:version]}.zip
+install_plugin "https://github.com/downloads/spinscale/elasticsearch-suggest-plugin/elasticsearch-suggest-#{node.elasticsearch[:plugin][:suggest][:version]}.zip suggest"
